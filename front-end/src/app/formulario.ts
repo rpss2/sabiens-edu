fe75316c3c9a1f362a426fa34b7bd73bb5b1622b
Ramom Pereira <rpss@cin.ufpe.br>
2017-12-03 21:06:03 -0200

    [#153347772]Funcionalidade para o cenario: estatisticas de um aluno

--- front-end/src/app/formulario.ts
+++ front-end/src/app/formulario.ts
@@ -2,33 +2,32 @@
 import { Material } from './material';
 import { Questao } from './questao';
 export class Formulario extends Material {
-  private _questoes: Questao[];
-  /*
-  .
-   */
-  constructor(nome: string, id: string, descricao: string) {
-    super(nome, id, descricao);
-  }
-  /*
-  .
-   */
-  public get questoes(): Questao[] {
-    return this._questoes;
-  }
+    private _questoes: Questao[];
+    /*
+    .
+     */
+    constructor(nome: string, id: string, descricao: string) {
+        super(nome, id, descricao);
+        this._questoes= [];
+    }
+    /*
+    .
+     */
+    public get questoes(): Questao[] {
+        return this._questoes;
+    }
 
-  public set questoes(value: Questao[]) {
-    this._questoes = value;
-  }
-  public adicionarQuestao(questao: Questao, i: number): boolean {
-    /*
-    adicionar coisas (isso é um esqueleto)
-     */
-    return true;
-  }
-  public removeQuestao(i: number): boolean {
-    /*
-    adicionar coisas (isso é um esqueleto)
-     */
-    return true;
-  }
+    public set questoes(value: Questao[]) {
+        this._questoes = value;
+    }
+    public adicionarQuestao(questao: Questao, i: number): boolean {
+        this._questoes[i] = questao;
+        return true;
+    }
+    public removeQuestao(i: number): boolean {
+        /*
+        adicionar coisas (isso é um esqueleto)
+         */
+        return true;
+    }
 }
