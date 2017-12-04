9797783efceeecc5620b95b5a3e8dc4068f510fa
Ramom Pereira <rpss@cin.ufpe.br>
2017-12-04 06:47:01 -0200

    [FINISHES #153351826]Funcionalidade do cenario: estatistica para aluno

--- front-end/src/app/estStudent.component.ts
+++ front-end/src/app/estStudent.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+import { EstatisticasService } from './estatisticas.service';
+import { Aluno } from './aluno';
+import { Escola } from './escola';
+import { Turma } from './turma';
+import { Sistemas } from './sistemas';
+import { FeedbackDeTurma } from './feedbackDeTurma';
+import {Formulario} from "./formulario";
+import {Resposta} from "./resposta";
+
 
 @Component({
     selector: 'app-root',
@@ -7,5 +18,47 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class EstStudentComponent implements OnInit {
-    ngOnInit(): void {}
+    constructor(private estatisticasService: EstatisticasService) {}
+
+    aluno : string;
+    sistema : string;
+
+    respostas : number[];
+    porcentagem : string;
+    flag : boolean = false;
+
+    getRespostas(aluno : string, sistema : string) : void {
+        this.respostas = [];
+        var respostas = this.estatisticasService.getRespostas(aluno, sistema);
+        if(respostas) {
+            this.respostas = respostas;
+        }
+    }
+
+    gerarEstatisticas(a : string, s : string) : void {
+        this.flag = false;
+        var value;
+        var cont = 0;
+        var formulario;
+        this.getRespostas(a, s);
+        if(this.respostas.length > 0) {
+            if (formulario = this.estatisticasService.getForms(s)) {
+                var questoes = formulario.questoes;
+                for (let key in questoes) {
+                    if (questoes[key].correta === this.respostas[key]) {
+                        cont++;
+                    }
+                }
+                value = (cont / questoes.length) * 100;
+                this.porcentagem = value + "%";
+                this.flag = true;
+            } else {
+                this.flag = false;
+            }
+        }
+    }
+
+    ngOnInit(): void {
+        this.estatisticasService.addDados();
+    }
 }
---
+++ tests/stepdefinitions/nenhumAlunoRespondeu.ts
@@ -0,0 +1,43 @@
+import { defineSupportCode } from 'cucumber';
+import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
+
+defineSupportCode(function ({ Given, When, Then, setDefaultTimeout }) {
+
+    setDefaultTimeout(60 * 1000);
+
+    Given(/^Eu estou na pagina de visualizacao de estatisticas para turmas$/, async () => {
+        await browser.get("http://localhost:4200");
+        //await expect(browser.getTitle()).to.eventually.equal('Feedback');
+        await $("a[name='class']").click();
+    })
+
+    Given(/^Nenhum aluno da turma "([^\"]*)" que pertence a escola "([^\"]*)" respondeu o formulario sobre "([^\"]*)"$/, async (turma, escola, sistema) => {
+        await $("input[name='escola']").sendKeys(<string> escola);
+        await $("input[name='turma']").sendKeys(<string> turma);
+        await $("input[name='sistema']").sendKeys(<string> sistema);
+        await element(by.buttonText('Listar Alunos')).click();
+        var allStudents : ElementArrayFinder = element.all(by.name('namelist'));
+        await allStudents;
+        await allStudents.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+    });
+
+    When(/^Eu tentar ver estatisticas sobre a turma "([^\"]*)"$/, async (turma) => {
+        await $("input[name='turma']").getText().then(text => text === turma);
+        //await expect($("input[name='turma']").getText()).toEqual(turma);
+        await element(by.buttonText('Ver Estatisticas')).click();
+    });
+
+    Then(/^Eu vejo uma mensagem de erro$/, async () => {
+        var alert = browser.switchTo().alert();
+        await expect(alert.getText()).to.eventually.equal("Nenhum aluno respondeu a esse questionario.");
+        await alert.dismiss();
+    });
+
+    Then(/^Continuo na mesma pagina$/, async () => {
+        await browser.get("http://localhost:4200/estClass");
+    });
+});
